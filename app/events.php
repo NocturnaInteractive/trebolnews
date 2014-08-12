07159c49eab500d693bb26275fea0b5639cc307f
Nartub <maimar@gmail.com>
2014-08-12 15:17:56 -0300

    dale

--- app/events.php
+++ app/events.php
@@ -6,22 +6,6 @@ Event::listen('auth.login', function($user) {
 });
 
 Event::listen('nuevo_registro', function($usuario) {
-    // try {
-    //     Mail::queue('emails/emailregistro', array(
-    //         'usuario' => $usuario
-    //     ), function($mail) use($usuario) {
-    //         $mail->from('info@trebolnews.com')
-    //              ->to($usuario->email)
-    //              ->subject('Bienvenido a TrebolNEWS');
-    //     });
-    // } catch (Exception $data) {
-    //     $usuario->delete();
-    //     return Response::json(array(
-    //         'status' => 'error',
-    //         'mensaje' => $data
-    //     ));
-    // }
-
     Carpeta::create(array(
         'id_usuario' => $usuario->id,
         'nombre' => 'basura'
