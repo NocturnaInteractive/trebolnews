7ddfe19117c1e67fe2c795e8c492aefd944c0f49
Nartub <maimar@gmail.com>
2014-08-12 21:19:05 -0300

    login event

--- app/events.php
+++ app/events.php
@@ -1,13 +1,21 @@
 <?php
 
-Event::listen('auth.login', function($user) {
-    $user->last_login = new DateTime;
-    $user->save();
+Event::listen('auth.login', function(Usuario $usuario) {
+	$usuario->last_login = new DateTime;
+	$usuario->save();
 });
 
-Event::listen('nuevo_registro', function($usuario) {
-    Carpeta::create(array(
-        'id_usuario' => $usuario->id,
-        'nombre' => 'basura'
-    ));
+Event::listen('nuevo_registro', function(Usuario $usuario) {
+	Mail::send('emails/emailregistro', array(
+		'usuario' => $usuario
+	), function($mail) use($usuario) {
+		$mail->from('info@trebolnews.com')
+			->to($usuario->email)
+			->subject('Bienvenido a TrebolNEWS');
+	});
+
+	Carpeta::create(array(
+		'id_usuario' => $usuario->id,
+		'nombre' => 'basura'
+	));
 });
