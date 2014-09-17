12de7e205cd1ac8282fada21dcea45e1fd5d8e59
Nartub <maimar@gmail.com>
2014-09-17 10:47:22 -0300

    minor, tests

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -2,26 +2,25 @@
 
 class HomeController extends BaseController {
 
-	/*
-	|--------------------------------------------------------------------------
-	| Default Home Controller
-	|--------------------------------------------------------------------------
-	|
-	| You may wish to use controllers instead of, or in addition to, Closure
-	| based routes. That's great! Here is an example controller method to
-	| get you started. To route to this controller, just add the route:
-	|
-	|	Route::get('/', 'HomeController@showWelcome');
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Default Home Controller
+    |--------------------------------------------------------------------------
+    |
+    | You may wish to use controllers instead of, or in addition to, Closure
+    | based routes. That's great! Here is an example controller method to
+    | get you started. To route to this controller, just add the route:
+    |
+    |   Route::get('/', 'HomeController@showWelcome');
+    |
+    */
 
-	public function index()
-	{
-		$res = array(
-				'plans' => Plan::all()
-			);
+    public function index() {
+        $res = array(
+            'plans' => Plan::all()
+        );
 
-		return View::make('home/index',$res);
-	}
+        return View::make('home/index', $res);
+    }
 
 }
