20f0a7e1cefabe580b17d42e3f9d1fb9891cbb28
Nartub <maimar@gmail.com>
2014-09-29 15:58:34 -0300

    db environments

--- app/config/digitalocean/database.php
+++ app/config/digitalocean/database.php
@@ -54,10 +54,10 @@ return array(
 
         'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => '',
-			'database'  => '',
-			'username'  => '',
-			'password'  => '',
+			'host'      => '104.131.97.126',
+			'database'  => 'trebolnews',
+			'username'  => 'forge',
+			'password'  => 'MDCqSRNH85i1o0jaKLvd',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
