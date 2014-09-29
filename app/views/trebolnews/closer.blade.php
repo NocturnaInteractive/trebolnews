90e755a75db1e35cb17ad7f876cfa5f404622537
Nartub <maimar@gmail.com>
2014-09-29 17:51:15 -0300

    FB login

--- app/views/trebolnews/closer.blade.php
+++ app/views/trebolnews/closer.blade.php
@@ -1,8 +1,14 @@
 <html>
 	<head>
-
+		{{ HTML::script('js/jquery-1.11.1.min.js') }}
+		<script>
+		$(function(){
+			window.opener.location.reload();
+			window.close();
+		});
+		</script>
 	</head>
 	<body>
-		{{ Session::all() }}
+
 	</body>
 </html>
