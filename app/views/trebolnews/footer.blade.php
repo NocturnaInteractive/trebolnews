ea149c64a4aaeacca75d99b3dec60f0e0a49e4b5
Nartub <maimar@gmail.com>
2014-10-06 12:46:06 -0300

    Complementa el commit anterior

--- app/views/trebolnews/footer.blade.php
+++ app/views/trebolnews/footer.blade.php
@@ -1,3 +1,35 @@
+<script>
+$(function(){
+    $('#subanewsletter input[type="submit"]').one('click', subscripcion_handler);
+
+    function subscripcion_handler(e) {
+        e.preventDefault();
+
+        $('#subanewsletter input[type="submit"]').on('click', function(e){
+            e.preventDefault();
+        });
+
+        $('#subanewsletter').ajaxSubmit({
+            success: function(data) {
+                if(data.status == 'ok') {
+                    noty({
+                        type: 'success',
+                        text: data.mensaje,
+                        layout: 'topCenter',
+                        timeout: 1000
+                    });
+                } else {
+                    notys(data.validator);
+                }
+            },
+            complete: function() {
+                $('#subanewsletter input[type="submit"]').one('click', subscripcion_handler);
+            }
+        });
+    }
+});
+</script>
+
 <div id="foo">
     <div id="foo_text">
         <div id="foo_izq">
@@ -7,7 +39,7 @@
         <div id="foo_der">
             <a href="#" class="twe">Seguinos por Tweeter</a>
             <a href="#" class="face">Estamos en Facebook</a>
-            <form id="subanewsletter" method="post" action=""> <!-- es necesario que coincida el nombre de este archivo php con el que aparece en el campo action -->
+            <form id="subanewsletter" method="post" action="{{ action('ExtraController@guardar_suscripcion') }}">
                 <input type="text" name="email" class="compo-form" id="newsletter" placeholder="Suscr&iacute;bete a nuestro Newsletter"  style=" color:#FFF; font-size:12px;"  />
                 <input type="submit" id="button" value="ENVIAR" />
             </form>
