95dabc0df57d9f7f8ce4a7fcb3326e5bcaadf084
agustinalmeida <agustinalmeida@gmail.com>
2014-10-27 17:11:22 -0300

    home 2

--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -23,7 +23,11 @@
 
     {{ HTML::script('js/trebolnews.js') }}
     {{ HTML::style('css/trebolnews.css') }}
+    {{ HTML::style('css/general.css') }}
 
+    <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+    <script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
+        
     <script>
         $(function() {
             $('#loginentrar').one('click', login_handler);
