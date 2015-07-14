80a74dfef2836c8c379f1ebdb3d50715abd3fec4
Martin Sacco <martin.sacco@globant.com>
2015-07-13 23:12:05 -0300

    try catch for send single mail

--- app/controllers/MailController.php
+++ app/controllers/MailController.php
@@ -59,25 +59,30 @@ class MailController extends \BaseController {
 	}
 
 	public function sendSingleMail($data){
-		Log::info('Setting Campaign...');
-    	$campaign = (object) $data['campaign'];
-		Log::info('Setting Contact...');
-    	$contacto = (object) $data['contact'];
-		Log::info('Setting View...');
-    	$campaignView = $this->getCampaignView($campaign);
-    	$campaignView->suscriptor->name  = $contacto->nombre;
-		$campaignView->suscriptor->last  = $contacto->apellido;
-		$campaignView->suscriptor->email = $contacto->email;
-		Log::info('Sending email...');
+		try {
+		    Log::info('Setting Campaign...');
+	    	$campaign = (object) $data['campaign'];
+			Log::info('Setting Contact...');
+	    	$contacto = (object) $data['contact'];
+			Log::info('Setting View...');
+	    	$campaignView = $this->getCampaignView($campaign);
+	    	$campaignView->suscriptor->name  = $contacto->nombre;
+			$campaignView->suscriptor->last  = $contacto->apellido;
+			$campaignView->suscriptor->email = $contacto->email;
+			Log::info('Sending email...');
 
-    	Mail::send('emails/campaign', array(
-			'campaign' => $campaignView
-		), function($mail) use($campaign, $contacto) {
-			$mail->to($contacto->email, "{$contacto->nombre} {$contacto->apellido}")
-				 ->subject($campaign->asunto)
-				 ->from($campaign->email, $campaign->remitente)
-				 ->replyTo($campaign->respuesta);
-		});
+	    	Mail::send('emails/campaign', array(
+				'campaign' => $campaignView
+			), function($mail) use($campaign, $contacto) {
+				$mail->to($contacto->email, "{$contacto->nombre} {$contacto->apellido}")
+					 ->subject($campaign->asunto)
+					 ->from($campaign->email, $campaign->remitente)
+					 ->replyTo($campaign->respuesta);
+			});
+		} catch (Exception $e) {
+		    echo 'Exception Found!! ',  $e->getMessage(), "\n";
+		}
+
 	}
 
 	public function getCampaignView($campaign){
