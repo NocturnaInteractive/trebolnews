221be943f59f0acad6bdf9f43251c78a1f4c98a2
Martin Sacco <martin.sacco@globant.com>
2015-08-03 20:56:13 -0300

    test mandrill API mailer

---
+++ app/config/services.php
@@ -0,0 +1,19 @@
+<?php
+
+return array(
+
+    'mailgun' => array(
+        'domain' => '',
+        'secret' => '',
+    ),
+
+    'mandrill' => array(
+        'secret' => 'EUsZf5kVhJGYHgSHY1V3wg',
+    ),
+
+    'stripe' => array(
+        'model'  => 'User',
+        'secret' => '',
+    ),
+
+);
