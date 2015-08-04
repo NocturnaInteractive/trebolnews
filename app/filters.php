ce5a9226559182e963d74da0464b6142f05853cc
Martin Sacco <martin.sacco@globant.com>
2015-08-04 00:18:17 -0300

    trying filters origin

--- app/filters.php
+++ app/filters.php
@@ -25,7 +25,10 @@ Route::filter('admin', function() {
 
 App::before(function($request)
 {
-    //
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+    header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
+    header('Access-Control-Allow-Credentials: true');
 });
 
 
