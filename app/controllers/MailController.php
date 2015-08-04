12975b547da507ada24cc5ee92927c15d18e1fa7
Martin Sacco <martin.sacco@globant.com>
2015-08-03 21:42:53 -0300

    testing queue

--- app/controllers/MailController.php
+++ app/controllers/MailController.php
@@ -78,7 +78,15 @@ class MailController extends \BaseController {
 				'campaign' => $campaignView
 			);
 			Log::info('Sending email...');
-
+			
+			Mail::queue('emails/campaign', $data, function($mail) use($campaign, $contacto) {
+				Log::info('Processing email...');
+				$mail->to($contacto->email, "{$contacto->nombre} {$contacto->apellido}")
+					 ->subject($campaign->asunto)
+					 ->from($campaign->email, $campaign->remitente)
+					 ->replyTo($campaign->respuesta);
+			});
+			
 	    	Mail::send('emails/campaign', $data, function($mail) use($campaign, $contacto) {
 				Log::info('Processing email...');
 				$mail->to($contacto->email, "{$contacto->nombre} {$contacto->apellido}")
