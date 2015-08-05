fad958b6e6ce50c4684870682a052b338337bcbb
Martin Sacco <martin.sacco@globant.com>
2015-08-05 01:35:53 -0300

    mail sent in queue

--- app/controllers/SendEmailQueue.php
+++ app/controllers/SendEmailQueue.php
@@ -4,7 +4,35 @@ class SendEmailQueue extends \BaseController {
 
     public function fire($job, $data) {
     	$mailController = new MailController();
-    	$mailController->sendSingleMail($data);
+
+    	Log::info('Setting Campaign...');
+    	$campaign = (object) $data['campaign'];
+		Log::info('Setting Contact...');
+    	$contacto = (object) $data['contact'];
+		Log::info('Setting View...');
+    	$campaignView = $this->getCampaignView($campaign);
+    	$campaignView->suscriptor->name  = $contacto->nombre;
+		$campaignView->suscriptor->last  = $contacto->apellido;
+		$campaignView->suscriptor->email = $contacto->email;
+
+		
+		Log::info('Setting Data...');
+		$data = array( 
+			'email' => 'maxi@nocturnainteractive.com', 
+			'async' => true, 
+			'campaign' => $campaignView
+		);
+		Log::info('Sending email...');
+
+    	Mail::send('emails/campaign', $data, function($mail) use($campaign, $contacto) {
+			Log::info('Processing email...');
+			$mail->to($contacto->email, "{$contacto->nombre} {$contacto->apellido}")
+				 ->subject($campaign->asunto)
+				 ->from($campaign->email, $campaign->remitente)
+				 ->replyTo($campaign->respuesta);
+		});
+		Log::info('Email Sent to '.$campaignView->suscriptor->email);
+
     	$job->delete();
 	}
 
