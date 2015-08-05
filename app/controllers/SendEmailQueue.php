5c65fecb73fd161a18e384b322dfda370c463d72
Martin Sacco <martin.sacco@globant.com>
2015-08-05 02:08:15 -0300

    alerts

--- app/controllers/SendEmailQueue.php
+++ app/controllers/SendEmailQueue.php
@@ -3,7 +3,15 @@
 class SendEmailQueue extends \BaseController {
 
     public function fire($job, $data) {
-    	Request::create('/mail/singlemail', 'POST', $data);
+    	try {
+		    Log::info('Making POST Request...');
+	    	Request::create('/mail/singlemail', 'POST', $data);
+		} catch (Exception $e) {
+			Log::info('Exception Found!! '. $e->getMessage());
+			Log::info($_SERVER);
+		    echo 'Exception Found!! ',  $e->getMessage(), "\n";
+		}
+    	
     	$job->delete();
 	}
 
