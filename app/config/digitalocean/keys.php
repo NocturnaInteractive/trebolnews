3f9cab89129ceb5f4a102fcbdd59e2ec7a866c80
Martin Sacco <martin.sacco@globant.com>
2015-09-18 13:01:27 -0300

    update facebook credentials

--- app/config/digitalocean/keys.php
+++ app/config/digitalocean/keys.php
@@ -3,8 +3,8 @@
 return array(
 
     'Facebook' => array(
-        'client_id'     => '594990277275305',
-        'client_secret' => 'c514359a8a07e246865bd7db870aa1fb',
+        'client_id'     => '259135847734',
+        'client_secret' => '9557673f051444ae6b263b1d875cf2ca',
         'scope'         => array('email'),
     )
 
