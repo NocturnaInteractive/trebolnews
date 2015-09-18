c7168cb10efd6b8afea5cde7ed56a383cd824e4b
Martin Sacco <martin.sacco@globant.com>
2015-09-18 13:12:23 -0300

    keys for facebook

---
+++ app/config/keys.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+
+    'Facebook' => array(
+        'client_id'     => '259135847734',
+        'client_secret' => '9557673f051444ae6b263b1d875cf2ca',
+        'scope'         => array('email'),
+    )
+
+);
